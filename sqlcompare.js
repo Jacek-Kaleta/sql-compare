60e0cdb6952cdba533c01f23c88ba510fbfdfd0e
Jacek-Kaleta <44475913+Jacek-Kaleta@users.noreply.github.com>
2022-09-23 09:47:52 +0200

    Add files via upload

--- sqlcompare.js
+++ sqlcompare.js
@@ -1,21 +1,7 @@
-/*var htmlTemplateStr = "<!DOCTYPE html>"
-+"\n<html lang='en'>"
-+"\n	<head>"
-+"\n		<meta charset='utf-8'>"
-+"\n		<title>Page Title</title>"
-+"\n	<head>"
-+"\n	<body>"
-+"\n		<!-- CONTENT GOES HERE. OK ? -->"
-+"\n	</body>"
-+"\n</html>";*/
-
 var codeEditor1 = document.getElementById('codeEditor1');
 var lineCounter1 = document.getElementById('lineCounter1');
-
 var codeEditor2 = document.getElementById('codeEditor2');
 var lineCounter2 = document.getElementById('lineCounter2');
-
-
 
 class TextEditor {
 	constructor(codeEditor, lineCounter) {
@@ -132,6 +118,11 @@ function compareText()
 			return v1.charAt(pos1).toLowerCase();
 		}
 		
+		function c_1()
+		{
+			return v1.charAt(pos1);
+		}
+		
 		function c1blank()
 		{
 			if (c1()==" ") return true;
@@ -146,12 +137,42 @@ function compareText()
 			return v2.charAt(pos2).toLowerCase();
 		}
 		
+		function c_2()
+		{
+			return v2.charAt(pos2);
+		}
+		
 		function c2blank()
 		{
 			if (c2()==" ") return true;
 			if (c2()=="\r") return true;
 			if (c2()=="\n") return true;
 			if (c2()=="\t") return true;
+			return false;
+		}
+		
+		
+		function checkchar(c)
+		{
+			if (c1() == c  && c2()==c)
+			{
+				pos1++;
+				pos2++;
+				while (pos1< code1len && c1blank()) pos1++;
+				while (pos2< code2len && c2blank()) pos2++;
+				return true;
+			} else
+			if (c1() == c  && c2blank())
+			{
+				while (pos2< code2len && c2blank()) pos2++;
+				return true;
+			} else
+			if (c2() == c && c1blank())
+			{
+				while (pos1< code1len && c1blank()) pos1++;
+				return true;
+			} 
+			else
 			return false;
 		}
 		
@@ -202,57 +223,44 @@ function compareText()
 				while (pos1< code1len && c1blank()) pos1++;
 				while (pos2< code2len && c2blank()) pos2++;
 			} else
-			if (c1()==',' && c2()==',')
-			{
-				pos1++;
-				pos2++;
-				while (pos1< code1len && c1blank()) pos1++;
-				while (pos2< code2len && c2blank()) pos2++;
-			} else
-			if (c1()==',' && c2blank())
-			{
-				while (pos2< code2len && c2blank()) pos2++;
-			} else
-			if (c2()==',' && c1blank())
-			{
-				while (pos1< code1len && c1blank()) pos1++;
-			} else
-			if (c1()=='(' && c2()=='(')
-			{
-				pos1++;
-				pos2++;
-				while (pos1< code1len && c1blank()) pos1++;
-				while (pos2< code2len && c2blank()) pos2++;
-			} else
-			if (c1()=='(' && c2blank())
-			{
-				while (pos2< code2len && c2blank()) pos2++;
-			} else
-			if (c2()=='(' && c1blank())
-			{
-				while (pos1< code1len && c1blank()) pos1++;
-			} else
-			if (c1()==')' && c2()==')')
-			{
-				pos1++;
-				pos2++;
-				while (pos1< code1len && c1blank()) pos1++;
-				while (pos2< code2len && c2blank()) pos2++;
-			} else
-			if (c1()==')' && c2blank())
-			{
-				while (pos2< code2len && c2blank()) pos2++;
-			} else
-			if (c2()==')' && c1blank())
-			{
-				while (pos1< code1len && c1blank()) pos1++;
-			} else
+			if (checkchar(','));else
+			if (checkchar('('));else
+			if (checkchar(')'));else
+			if (checkchar('<'));else
+			if (checkchar('>'));else
+			if (checkchar('='));else
+			if (checkchar('|'));else
 			if (c1blank() && c2blank())
 			{
 				while (pos1< code1len && c1blank()) pos1++;
 				while (pos2< code2len && c2blank()) pos2++;
 			}
 			else
+			if (c1 () == '\'' && c2 () == '\'')
+			{
+				console.log('Tu');
+				pos1++;
+				pos2++;
+				while (pos1 < code1len && pos2 < code2len && c_1() == c_2() && c_1() != '\'')
+				{
+					pos1++;
+					pos2++;
+				}
+				if (pos1< code1len && c_1() =='\'' && pos2< code2len && c_2() =='\'')
+				{
+					pos1++;
+					pos2++;
+				} ;
+				if ( pos1 < code1len)
+				if ( pos2 < code2len)
+				if (c1blank() && c2blank()) ;
+				else
+				if ( c_1 ()!=  c_2() )
+				{
+					scroll();
+					return ;
+				}
+			} else
 			if (c1 ()!=  c2())
 			{
 				/*while ( pos1 < code1len -1 && v1.substr(pos1,2)=='--')
